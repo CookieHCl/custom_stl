c24899585881ba73bdd33d177e81100413159381
CookieHCl <nhc7502@snu.ac.kr>
2026-02-03 03:04:20 +0900

    아 똑바로 보고 커밋하라고

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CUSTOM_STDL
+# CUSTOM_STL
 
 - [10828](https://www.acmicpc.net/problem/10828): stack
 - [10845](https://www.acmicpc.net/problem/10845): queue
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include "set.h"
-
 int main()
 {
 	iostream::sync_with_stdio(0);
 	cin.tie(0);
 
-	my_set<int> s(true);
-
-	int n, t, x;
-	cin >> n;
-
-	while (n--)
-	{
-		cin >> t >> x;
-		if (t == 1)
-		{
-			s.insert(x);
-		}
-		else
-		{
-			int k = *(s.kth(x));
-			s.erase_one(k);
-			cout << k << '\n';
-		}
-	}
-
+	cout << "Hello world!";
 	return 0;
 }
